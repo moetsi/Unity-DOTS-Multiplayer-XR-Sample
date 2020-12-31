baf8d5eda341de24ebcaf3d7d0a7ae27fb76fe37
Adam Polak <adammac@Adams-MacBook-Pro-2.local>
2020-12-30 22:28:00 -0500

    End code state after *Setting up a project for ECS* in Unity DOTS Multiplayer XR Sample Project gitbook

---
+++ Assets/Scripts and Prefabs/GameSettingsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+
+public struct GameSettingsComponent : IComponentData
+{
+    public float asteroidVelocity;
+    public float playerForce;
+    public float bulletVelocity;
+    public int numAsteroids;
+    public int levelWidth;
+    public int levelHeight;
+    public int levelDepth;
+}
---
+++ Assets/Scripts and Prefabs/SetGameSettingsSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SetGameSettingsSystem : UnityEngine.MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float asteroidVelocity = 10f;
+    public float playerForce = 50f;
+    public float bulletVelocity = 500f;
+    public int numAsteroids = 200;
+    public int levelWidth = 2048;
+    public int levelHeight = 2048;
+    public int levelDepth = 2048;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var settings = default(GameSettingsComponent);
+        settings.asteroidVelocity = asteroidVelocity;
+        settings.playerForce = playerForce;
+        settings.bulletVelocity = bulletVelocity;
+        settings.numAsteroids = numAsteroids;
+        settings.levelWidth = levelWidth;
+        settings.levelHeight = levelHeight;
+        settings.levelDepth = levelDepth;
+        dstManager.AddComponentData(entity, settings);
+    }
+}
