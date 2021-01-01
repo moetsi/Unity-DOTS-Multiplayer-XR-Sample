4254f3c1faf8906bfee7c8b574e6e38b23d93eb8
Adam Polak <adammac@Adams-MacBook-Pro-2.local>
2020-12-31 22:53:43 -0500

    Spawning-Updating-and-Destroying-Asteroids section in gitbook

---
+++ Assets/Scripts and Prefabs/MovementSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Unity.Burst;
+
+public class MovementSystem : SystemBase
+{
+    [BurstCompile]
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        Entities
+        .ForEach((ref Translation position, in VelocityComponent velocity) =>
+        {
+            position.Value.xyz += velocity.Value * deltaTime;
+        }).ScheduleParallel();
+    }
+}
---
+++ Assets/Scripts and Prefabs/VelocityComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct VelocityComponent : IComponentData
+{
+    public float3 Value;
+}
