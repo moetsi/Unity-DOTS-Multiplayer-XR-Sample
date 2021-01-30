24e8ae9b21f217665d198ae7befffcec1ad06595
Adam Polak <adammac@Adams-MacBook-Pro-2.local>
2021-01-29 20:05:21 -0500

    Code state at end of Setting-Up-AR-Foundation

---
+++ Assets/Scripts and Prefabs/Client/Components/IsARPlayerComponent.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public struct IsARPlayerComponent : IComponentData
+{
+}
---
+++ Assets/Scripts and Prefabs/Mixed/ARPlatformInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using Unity.Entities;
+using Unity.NetCode;
+
+public class ARPlatformInitializer : MonoBehaviour
+{
+    [SerializeField] GameObject m_Session;
+    [SerializeField] GameObject m_SessionOrigin;
+
+    IEnumerator Start() {
+        if ((ARSession.state == ARSessionState.None) ||
+            (ARSession.state == ARSessionState.CheckingAvailability))
+        {
+            yield return ARSession.CheckAvailability();
+        }
+
+        if (ARSession.state == ARSessionState.Unsupported)
+        {
+            //If we AR is unsupported we disable both GameObjects
+            m_SessionOrigin.SetActive(false);
+            m_Session.SetActive(false);
+        }
+        else
+        {
+            //If AR is supported we create our IsARPlayerComponent singleton in ClientWorld
+            foreach (var world in World.All)
+            {
+                if (world.GetExistingSystem<ClientSimulationSystemGroup>() != null)
+                {
+                    world.EntityManager.CreateEntity(typeof(IsARPlayerComponent));
+                }
+            }
+
+        }
+    }
+}
