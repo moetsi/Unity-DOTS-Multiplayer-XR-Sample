270b36b12e9e02901e8617ab8af3a2398e271f34
adammpolak <adammpolak@gmail.com>
2022-06-17 21:29:54 -0400

    commit for Styling-a-view

--- Assets/UI/HostGameScreen.cs
+++ Assets/UI/HostGameScreen.cs
@@ -1,9 +1,26 @@
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine.SceneManagement;
 
 public class HostGameScreen : VisualElement
 {
-    
+    TextField m_GameName;
+    TextField m_GameIp;
+    TextField m_PlayerName;
+    String m_HostName = "";
+    IPAddress m_MyIp;
+
     public new class UxmlFactory : UxmlFactory<HostGameScreen, UxmlTraits> { }
 
     public HostGameScreen()
@@ -13,8 +30,24 @@ public class HostGameScreen : VisualElement
 
     void OnGeometryChange(GeometryChangedEvent evt)
     {
-        
+        // 
+        // PROVIDE ACCESS TO THE FORM ELEMENTS THROUGH VARIABLES
+        // 
+        m_GameName = this.Q<TextField>("game-name");
+        m_GameIp = this.Q<TextField>("game-ip");
+        m_PlayerName = this.Q<TextField>("player-name");
+
+        //  CLICKING CALLBACKS
+        this.Q("launch-host-game")?.RegisterCallback<ClickEvent>(ev => ClickedHostGame());
+
 
         this.UnregisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
+
+    void ClickedHostGame()
+    {
+
+        Debug.Log("clicked host game");
+    }
+
 }
--- Assets/UI/ManualConnectScreen.cs
+++ Assets/UI/ManualConnectScreen.cs
@@ -1,9 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 public class ManualConnectScreen : VisualElement
 {
-    
+    TextField m_GameIp;
+    TextField m_PlayerName;
+    string m_HostName = "";
+    IPAddress m_MyIp;
+
     public new class UxmlFactory : UxmlFactory<ManualConnectScreen, UxmlTraits> { }
 
     public ManualConnectScreen()
@@ -13,8 +22,21 @@ public class ManualConnectScreen : VisualElement
 
     void OnGeometryChange(GeometryChangedEvent evt)
     {
-        
+        // 
+        // PROVIDE ACCESS TO THE FORM ELEMENTS THROUGH VARIABLES
+        // 
+        m_GameIp = this.Q<TextField>("game-ip");
+        m_PlayerName = this.Q<TextField>("player-name");
+
+        //  CLICKING CALLBACKS
+        this.Q("launch-connect-game")?.RegisterCallback<ClickEvent>(ev => ClickedJoinGame());
 
         this.UnregisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
+
+    void ClickedJoinGame()
+    {
+        Debug.Log("clicked manual connect");
+    }
+
 }
